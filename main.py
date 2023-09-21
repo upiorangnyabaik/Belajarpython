f6d4d77579d5f2dd507de1830b5a5e2b329fe096
upiorangnyabaik <145647882+upiorangnyabaik@users.noreply.github.com>
2023-09-21 07:48:12 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+hari_ini ="sabtu"
+
+
+if(hari_ini== "senin"):
+   print("saya pergi sekolah")
+elif(hari_ini=="selasa"):
+   print("saya pergi sekolah")
+elif(hari_ini=="rabu"):
+   print("saya pergi sekolah")
+elif(hari_ini=="Kamis"):
+   print("saya pergi sekolah")
+elif(hari_ini=="jumat"):
+   print("saya pergi jumatan")
+elif(hari_ini=="sabtu"):
+   print("saya malam mingguan")
+elif(hari_ini=="minggu"):
+   print("saya tidur dirumah")
+else:
+  print("hari_ini")
+   
