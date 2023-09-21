bce38f3ec3d268a0ee676c3b2355e20112a8f941
upiorangnyabaik <145647882+upiorangnyabaik@users.noreply.github.com>
2023-09-21 07:47:11 +0000

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,9 @@
+nama = input("Masukkan nama : ")
+alamat = input("Masukkan alamat : ")
+tgl_lahir = input("Masukkan tanggal lahir ")
+umur = input("Masukkan umur : ")
+
+print("Nama:", nama)
+print("Alamat:", alamat)
+print("Tanggal Lahir:", tgl_lahir)
+print("Umur:", umur, "tahun")
